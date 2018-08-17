e3e53a087f5f6e09e8f64d11932a2c711a394ab2
Kamil Szczypior <kszczypior1@gmail.com>
2018-08-17 20:57:43 +0200

    Flickity v1.0

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 
 
 // Flickity
-
+var restartButton = document.getElementById('first-cell');
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  
+var progressBar = document.querySelector('.progress-bar')
+// Add flickity
+var flkty = new Flickity( elem, {   
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  hash: true,
+  pageDots: false,  
+});
+// progress bar
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+// restart button
+restartButton.addEventListener("click", function(){
+    flkty.select(0);
 });
 
 // element argument can be a selector string
